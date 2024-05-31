2b7be390990e4178995433ce0cc45d7c74154de3
Shavius <glamurparty@ukr.net>
2024-05-31 23:08:46 +0300

    В класс CardUpdate подключил класс DateControl

--- src/ts/modules/CardUpdate.ts
+++ src/ts/modules/CardUpdate.ts
@@ -1,3 +1,5 @@
+import DateControl from "./DateControl";
+
 type params = "up" | "down";
 
 export default class CardUpdate {
@@ -5,6 +7,7 @@ export default class CardUpdate {
 	nameCurrentSeria: string = ".serial-info__number-current span";
 	nameAllSeria: string = ".serial-info__number-all span";
 	nameLeftSeria: string = ".serial-info__number-left span";
+	nameDateUpdate: string = ".card-date__item-update span";
 
 	constructor(currentCard: HTMLElement | null) {
 		this.currentCard = currentCard;
@@ -15,10 +18,16 @@ export default class CardUpdate {
 			const currentSeria: HTMLElement | null = this.currentCard.querySelector(this.nameCurrentSeria);
 			const allSeria: HTMLElement | null = this.currentCard.querySelector(this.nameAllSeria);
 			const leftSeria: HTMLElement | null = this.currentCard.querySelector(this.nameLeftSeria);
+			const dateUpdate: HTMLElement | null = this.currentCard.querySelector(this.nameDateUpdate);
 
 			if (currentSeria !== null) {
 				const currentSeriaString: string = currentSeria.innerHTML;
 				let currentSeriaNumber: number = Number(currentSeriaString);
+
+				if (dateUpdate !== null) {
+					const currentDate = new DateControl().getCurrentDate();
+					dateUpdate.innerHTML = `${currentDate}`;
+				}
 
 				if (buttonParams === "up") {
 					currentSeriaNumber += 1;
