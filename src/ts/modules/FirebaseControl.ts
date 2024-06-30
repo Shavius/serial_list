d7d0069768d7267d2a5c2a05cf993fae1a29f17b
Shavius <glamurparty@ukr.net>
2024-06-30 15:36:58 +0300

    Обнавил Класс FirebaseControl добавил два метода
    - Добавил Метод sendDataToDatabase
    - Добавил Метод getDataFromDatabase

--- src/ts/modules/FirebaseControl.ts
+++ src/ts/modules/FirebaseControl.ts
@@ -1,5 +1,7 @@
 /* eslint-disable class-methods-use-this */
 
+import IDataCard from "../interfaces/IDataCard";
+
 export default class FirebaseControl {
 	loginWithEmailPassword = async (email: string, password: string, apiKey: string): Promise<string> => {
 		const url = `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${apiKey}`;
@@ -32,5 +34,53 @@ export default class FirebaseControl {
 		console.log(idToken);
 
 		return idToken;
+	};
+
+	sendDataToDatabase = async (idToken: string, data: IDataCard[]): Promise<boolean> => {
+		const url = `https://shava-serial-list-default-rtdb.firebaseio.com/user.json?auth=${idToken}`;
+
+		try {
+			const response = await fetch(url, {
+				method: "PUT",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify(data),
+			});
+
+			if (!response.ok) {
+				throw new Error("Ошибка отправки данных");
+			}
+
+			console.log("Данные успешно отправлены:", data);
+			return true;
+		} catch (error) {
+			console.error("Ошибка:", error);
+			return false;
+		}
+	};
+
+	getDataFromDatabase = async (idToken: string): Promise<string | null> => {
+		const url = `https://shava-serial-list-default-rtdb.firebaseio.com/user.json?auth=${idToken}`;
+
+		try {
+			const response = await fetch(url, {
+				method: "GET",
+				headers: {
+					"Content-Type": "application/json",
+				},
+			});
+
+			if (!response.ok) {
+				throw new Error("Ошибка получения данных");
+			}
+
+			const data: string = await response.json();
+			console.log("Данные из базы данных:", data);
+			return data;
+		} catch (error) {
+			console.error("Ошибка:", error);
+			return null;
+		}
 	};
 }
