658871885900ca86c8e857b8f5c492a09ba3bc0c
Shavius <glamurparty@ukr.net>
2024-09-20 02:27:53 +0300

    Восстановление положения прокрутки после перезагрузки (#29)

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -3,6 +3,10 @@ import "../404.html";
 import "../scss/main.scss";
 import ControlButtons from "./modules/ControlButtons";
 import UserCheck from "./modules/UserCheck";
+import ScrollingAfterReboot from "./modules/ScrollingAfterReboot";
+
+const scrollingAfterReboot = new ScrollingAfterReboot();
+scrollingAfterReboot.saveScrollPositionBeforeUnload();
 
 const controlButtons = new ControlButtons();
 controlButtons.init();
